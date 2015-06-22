dcf6e54d1c610b8bb44a26311115401079ac0c7b
Will Jordan <will@code.org>
2015-06-21 19:49:48 -0700

    add typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="async/async.d.ts" />
+/// <reference path="filesystem/filesystem.d.ts" />
+/// <reference path="filewriter/filewriter.d.ts" />
+/// <reference path="dropboxjs/dropboxjs.d.ts" />
+/// <reference path="node/node-0.10.d.ts" />
